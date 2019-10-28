d94ba05ef5e9f02974f1cb53d918045e2835d4a1
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2019-10-28 13:36:04 -0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a clone of `git clone https://github.com/frewinchristopher/react-redux-shopify-storefront-api-example.git`
+
+But I'm going to mess with it a lot, so it may or may not be functioning.
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Client from 'shopify-buy';
+import { Provider } from 'react-redux';
+import store from './store';
+import './styles/shopify.css';
+
+// build shopify client
+const client = Client.buildClient({
+  storefrontAccessToken: process.env.REACT_APP_SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+  domain: `${process.env.REACT_APP_SHOP_NAME}.myshopify.com`,
+});
+store.dispatch({type: 'CLIENT_CREATED', payload: client});
+
+// buildClient() is synchronous, so we can call all these after!
+client.product.fetchAll().then((res) => {
+  store.dispatch({type: 'PRODUCTS_FOUND', payload: res});
+});
+client.checkout.create().then((res) => {
+  store.dispatch({type: 'CHECKOUT_FOUND', payload: res});
+});
+client.shop.fetchInfo().then((res) => {
+  store.dispatch({type: 'SHOP_FOUND', payload: res});
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,  
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
